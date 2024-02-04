0b3ca029e8ef0bb9c43ae3fb23291d1809704448
krebso <krebso@krebso.pl>
2024-02-04 18:45:52 +0100

    difficulty, filter, standarization, --no-output

--- eu4achievements.py
+++ eu4achievements.py
@@ -16,8 +16,12 @@ def privateCollectAchievements(page):
     for i, r in enumerate(achievementContainer):
         textContainer = r.find('div', class_='achieveTxt')
 
-        title = textContainer.find('h3').text
-        description = textContainer.find('h5').text
+        title = textContainer.find('h3').text.strip().replace('…', '...').replace('’', "'")
+
+        if (title == 'Brentry'):
+            title = 'Brentry!'
+
+        description = textContainer.find('h5').text.strip()
 
         unlocked = False
         if r.find('div', class_='achieveUnlockTime'):
@@ -58,15 +62,19 @@ def collectAchievementsDifficulty():
     soup = BeautifulSoup(page, features='lxml')
 
     achievements = []
+    # achievements = {}
 
-    for i, r in enumerate(soup.body.find_all('tr')):
+    table = soup.body.find('table')
+
+    for i, e in enumerate(table.find_all('tr')):
         if i == 0:
             continue
 
-        # title = r.find('td').text
-        # difficulty = r.find_all('td')[1].text
-
-        # achievements.append({'title': title, 'difficulty': difficulty})
+        title = e.find('span').get('id').replace('_', ' ').strip().replace('…', '...').replace('’', "'")
+        difficulty = e.text.strip().split('\n')[-1].strip()
+        
+        achievements.append({'title': title, 'difficulty': difficulty})
+        # achievements['title'] = difficulty
 
     return achievements
 
@@ -94,18 +102,26 @@ def parseArgs():
                         action='store_true',
                         default=False,
                         help='enable verbose logging')
+    parser.add_argument('--no-output',
+                        action='store_true',
+                        default=False,
+                        help='disable output')
 
     return parser.parse_args()
 
 def formattedLink(title):
-    return f'https://eu4.paradoxwikis.com/Achievements#List_of_achievements#:~:text={quote(title)}'
+    return f'https://eu4.paradoxwikis.com/Achievements#{title.replace(" ", "_")}'
 
 def formattedResult(achievement, link=None):
-    if link:
-        return f'{achievement["title"]}: {achievement["description"]}\n|> {formattedLink(achievement["title"])}'
+    try:
+        if link:
+            return f'[{achievement["difficulty"]}] {achievement["title"]}:\n|> {achievement["description"]}\n|> {formattedLink(achievement["title"])}'
 
-    return f'{achievement["title"]}: {achievement["description"]}'
+        return f'[{achievement["difficulty"]}] {achievement["title"]}:\n|> {achievement["description"]}'
     
+    except KeyError:
+        logger.error(f'KeyError: {achievement}')
+        return f'KeyError: {achievement}'
 
 def main():
     args = parseArgs()
@@ -115,6 +131,7 @@ def main():
     pickRandom = args.random
     link = args.link
     verbose = args.verbose
+    noOutput = args.no_output
 
     if verbose:
         logging.basicConfig(level=logging.INFO)
@@ -122,6 +139,11 @@ def main():
     logger.info(f'filter: {filter}')
 
     achievements = collectAchievements(user)
+    difficulties = collectAchievementsDifficulty()
+
+    logger.info(f'Merging achievements with difficulties')
+    lookup = {item['title']: item for item in difficulties}
+    achievements = [{**item, **lookup.get(item['title'], {})} for item in achievements]
 
     if achievements == []:
         print(f'No achievements found for the user: {user}...')
@@ -134,29 +156,38 @@ def main():
         if 'not-completed' in filter or 'nc' in filter:
             achievements = [a for a in achievements if not a['unlocked']]
             logger.info(f'Filtered for not completed achievements, found: {len(achievements)}')
+
+        allowedDifficulties = []
         if 'very-easy' in filter or 've' in filter:
-            pass
+            allowedDifficulties.append('VE')
         if 'easy' in filter or 'e' in filter:
-            pass
+            allowedDifficulties.append('E')
         if 'medium' in filter or 'm' in filter:
-            pass
+            allowedDifficulties.append('M')
         if 'hard' in filter or 'h' in filter:
-            pass
+            allowedDifficulties.append('H')
         if 'very-hard' in filter or 'vh' in filter:
-            pass    
+            allowedDifficulties.append('VH')
         if 'insane' in filter or 'i' in filter:
-            pass
+            allowedDifficulties.append('I')
         if 'uncategorized' in filter or 'uc' in filter:
-            pass
+            allowedDifficulties.append('UC')
+
+        if allowedDifficulties:
+            achievements = [a for a in achievements if a['difficulty'] in allowedDifficulties]
+            logger.info(f'Filtered for difficulties: {allowedDifficulties}, found: {len(achievements)}')
 
     if pickRandom:
         achievement = random.choice(achievements)
-        print(formattedResult(achievement, link))
+        text = formattedResult(achievement, link)
+        if not noOutput:
+            print(text)
     else:
         print(f'Total: {len(achievements)}')
         for achievement in achievements:
-            print(formattedResult(achievement, link))
-
+            text = formattedResult(achievement, link)
+            if not noOutput:
+                print(text)
 
 if __name__ == '__main__':
     main()
